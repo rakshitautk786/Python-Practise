ba21eab3dd74600a95e5212aabed2d912c9f8551
Rakshita Pawar <64592990+rakshitautk786@users.noreply.github.com>
2021-05-29 15:47:48 +0530

    Add files via upload

---
+++ Advance Python/enumerate_function.py
@@ -0,0 +1,5 @@
+''' Print 3rs 5th and 7th element of list'''
+l=[1,2,3,4,5,6,7,8,9,10]
+for index,item in enumerate(l):
+    if index==2 or index==4 or index==6:
+        print(f"the {index+1}th element is  {item}")
